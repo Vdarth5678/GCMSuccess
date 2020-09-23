8739219b8071b65bb0e382d93af1668b2fc97096
Sam2541 <69310265+Sam2541@users.noreply.github.com>
2020-09-23 23:17:22 +0530

    Add files via upload

--- telegram_gcloner/telegram_gcloner.py
+++ telegram_gcloner/telegram_gcloner.py
@@ -119,7 +119,7 @@ def main():
     load_handlers(updater.dispatcher)
 
     updater.start_polling()
-    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Telegram gcloner first call, start.')
+    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Telegram fcloner first call, start.')
     updater.idle()
 
 
